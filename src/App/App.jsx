caae7e6696f5b1db370b91e954451a7b51a27ad1
Susana Santos Moreno <susiylola@gmail.com>
2019-08-26 12:54:03 +0200

    Styles with Material Desing and Bootstrap

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import './App.css';
+import 'typeface-roboto';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const wishes = [
     { text : 'Travel to the moon', done : false},
@@ -6,23 +9,21 @@ const wishes = [
     { text : 'Go to the gym', done : false},
 ];
 const App = () => 
-    <div>
-        <h1>My wishlist app</h1>
-        <fieldset>
-            <legend>New Wish</legend>
-            <input placeholder="Enter your wish here"/>
+    <div className="app card">
+        <h1 className="card-title title" >My wishlist app</h1>
+        <fieldset className="form-group">
+            <legend className="card-subtitle mb-2 text-muted">New Wish</legend>
+            <input placeholder="Enter your wish here" className="form-control"/>
         </fieldset>
-        <ul>
+        <ul className="wish-list">
             {wishes.map(({text, done}, i) => (
-                <li>
-                    <label htmlFor={`wish${i}`}>
-                        <input id={`wish${i}`} type="checkbox" checked={done}></input>
-                        {text}
-                    </label>
+                <li key={text} className={`${done ? 'wish-list_item--done' : ''}`}>
+                    <Checkbox checked={done} id={`wish${i}`}/>
+                    <label htmlFor={`wish${i}`} className="form-check-label">{text}</label>
                 </li>
             ))}
         </ul>
-        <button type="button">Archive done</button>
+        <button type="button" className="btn btn-info">Archive done</button>
     </div>
 ;
 export default App;
